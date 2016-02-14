288643a97e43ed2e8b31ea99bdeab159bb709dfa
Md. Abdul Mazed Rupok <rupurup@gmail.com>
2016-02-14 01:21:42 -0800

    Update segment tree for query minimum O(logn).cpp

--- segment tree for query minimum O(logn).cpp
+++ segment tree for query minimum O(logn).cpp
@@ -6,7 +6,7 @@ int mnm[20];
 int num[] = {0, -1, 3, 4, 0, 2, 1, 2, 6};
 void build (int at, int L, int R) { //build array in O(log2n) ..
     if (L == R) {
-        mnm[at] = min(mnm[at], num[L]);
+        mnm[at] = num[L];
         return;
     }
     int mid = (L+R) /2;
@@ -34,7 +34,7 @@ int query (int at, int L, int R, int l, int r) { ////search in range [1,5].. in 
 }
 int main ()
 {
-    memset (mnm, 1e9, sizeof (mnm)); //all indexing to maximum value..
+    //memset (mnm, 1e9, sizeof (mnm)); //all indexing to maximum value..not necessary..
     build (1, 1, 8);        //building mnm array.. take memory 2n or 2(n+1)+1
     for (int i = 0; i < 20; i++) cout << mnm[i] << " ";
     cout << endl;
