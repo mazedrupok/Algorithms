bb2d3c2f72c0061d3a2c0b0fda85f6d704461de0
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-05-23 23:21:41 +0600

    Floyd Warshal Algorithm

---
+++ Floyd Warshal Algorithm.cpp
@@ -0,0 +1,73 @@
+/*
+   Name:  Md. Abdul Mazed
+   Noakhali Science & Technology University
+   Email:  mazedrupok@gmail.com || mazed4g@gmail.com
+   FB id:  www.facebook.com/mazedrupok
+   github: www.github.com/mazedrupok
+
+   Catagory: Graph (Floyd Warshal Algorithm)
+   Comment:  Finding All Pairs Shortest Path.
+   Source: Geeksforgeek
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define MX  30050
+typedef long long LL;
+#define V 4
+#define INF 1e9
+
+void printSolution(int dist[][V]);
+void floydWarshall (int graph[][V]) {
+    int dist[V][V], i, j, k;
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    for (k = 0; k < V; k++){
+        for (i = 0; i < V; i++){
+            for (j = 0; j < V; j++){
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V])
+{
+    printf ("Following matrix shows the shortest distances"
+            " between every pair of vertices \n");
+    for (int i = 0; i < V; i++){
+        for (int j = 0; j < V; j++){
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf ("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// driver program to test above function
+int main()
+{
+    int graph[V][V] = { {0,   5,  INF, 10},
+                        {INF, 0,   3, INF},
+                        {INF, INF, 0,   1},
+                        {INF, INF, INF, 0}
+                      };
+
+    floydWarshall(graph);
+    return 0;
+}
+    /* Let us create the following weighted graph
+            10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3
+    */
